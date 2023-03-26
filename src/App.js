ece083504e74d99d49aec447db2e591128459956
Human Interface Device <83428954+HumanInterfaceDevices@users.noreply.github.com>
2023-03-26 13:21:26 -0700

    First commit: success

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useMemo, useEffect } from "react";
+import { Canvas, useFrame, extend, useThree } from "@react-three/fiber";
+import { BoxGeometry, Vector3, EdgesGeometry, LineBasicMaterial, MeshStandardMaterial, BoxBufferGeometry, Mesh, Line } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-function App() {
+extend({ OrbitControls });
+
+const Cube = ({ position }) => {
+  const ref = useRef();
+  const edges = useMemo(() => new EdgesGeometry(new BoxGeometry(1, 1, 1)), []);
+
+  useFrame((state) => {
+    ref.current.position.y = Math.sin(state.clock.getElapsedTime() + position.x * 2 + position.z * 2) * 0.3;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <group ref={ref} position={position}>
+      <mesh geometry={new BoxBufferGeometry(1, 1, 1)} material={new MeshStandardMaterial({ transparent: true, opacity: 0.5 })} />
+      <line geometry={edges} material={new LineBasicMaterial({ linewidth: 2, transparent: true, opacity: 0.7, color: "cyan" })} />
+    </group>
   );
-}
+};
+
+const Controls = () => {
+  const { camera, gl } = useThree();
+  const controls = useRef();
+
+  useEffect(() => {
+    controls.current.addEventListener("change", gl.forceRender);
+    return () => controls.current.removeEventListener("change", gl.forceRender);
+  }, [gl]);
+
+  return <orbitControls ref={controls} args={[camera, gl.domElement]} />;
+};
+
+const App = () => {
+  const cubes = [];
+
+  for (let x = 0; x < 5; x++) {
+    for (let z = 0; z < 5; z++) {
+      cubes.push(<Cube key={`${x}-${z}`} position={new Vector3(x - 2, 0, z - 2)} />);
+    }
+  }
+
+  return (
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <React.Fragment>{cubes}</React.Fragment>
+      <Controls />
+    </Canvas>
+  );
+};
 
 export default App;
